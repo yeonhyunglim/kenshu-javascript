98c8ce27109985708b9dae00cb9ab2d18829b9c6
DESKTOP-N4LQJKL\dusgu <dusgud711@naver.com>
2021-11-30 15:00:56 +0900

    practice02(HTMLelementの持つ属性を取得する)を実装しました！

--- practice/02/try/src/js/index.js
+++ practice/02/try/src/js/index.js
@@ -1,12 +1,18 @@
 (function() {
     'use strict';
 
-    const button = document.getElementById('button'); // ここで定数がIDと一緒でも大丈夫ですか？
-    const textType = document.getElementsByClassName('text--type')[0];
-    const textClass = document.getElementsByClassName('text--class')[0];
+    const button = document.getElementById('button'); // <button id="button" data-type="register" class="btn">登録</button>
+    const textType = document.getElementsByClassName('text--type')[0]; // <p class="text--type"></p>
+    const textClass = document.getElementsByClassName('text--class')[0]; // <p class="text--class"></p>
 
     button.addEventListener('click', () => { 
-        textType.textContent = /* `data-type` 属性（"register"） */ ;
-        textClass.textContent = /* `class名` ("btn")*/ ;
+        textType.textContent = button.getAttribute('data-type');
+        /* Set the element's text content.
+        The HTML for text--type is now:
+        <p class="text--type">register</p> */
+        textClass.textContent = button.getAttribute('class');
+        /* Set the element's text content.
+        The HTML for text--type is now:
+        <p class="text--class">btn</p> */
     }) 
 })();
