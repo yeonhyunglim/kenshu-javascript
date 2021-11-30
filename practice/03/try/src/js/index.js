7ac966781335830768b794203a2b02bfe08d12fe
DESKTOP-N4LQJKL\dusgu <dusgud711@naver.com>
2021-11-30 18:39:14 +0900

    "is-acitve"から"is-active"へspelling errorを修正しました

--- practice/03/try/src/js/index.js
+++ practice/03/try/src/js/index.js
@@ -5,19 +5,17 @@
     const modalContent = document.getElementById('modal-content');
     const closeButton = document.getElementById('modal-close');
 
-    function toggleClass() {
-        modalOverlay.classList.toggle("is-active");　// 'is-active'にしたらＮＧ
-        modalContent.classList.toggle("is-acitve");
+    function toggleClass () {
+        modalOverlay.classList.toggle('is-active');
+        modalContent.classList.toggle('is-active');
     };
 
-    button.addEventListener('click', toggleClass); // "click"にしたらＮＧ　
+    button.addEventListener('click', toggleClass); 
     modalOverlay.addEventListener('click', toggleClass);
     closeButton.addEventListener('click', toggleClass);
-
-})();
+})(); 
 
 /* 他の方法として
-
     button.addEventListener('click', () => {
         modalOverlay.classList.add("is-active");
         modalContent.classList.add("is-active");
@@ -27,10 +25,8 @@
         modalOverlay.classList.remove("is-active");
         modalContent.classList.remove("is-active");
     });
-
     closeButton.addEventListener('click', () => {
         modalOverlay.classList.remove("is-active");
         modalContent.classList.remove("is-active");
     });
-
 もできる */    
