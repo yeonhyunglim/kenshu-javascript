af02df2347356296f8deebee5fb822c0527bb8ce
DESKTOP-N4LQJKL\dusgu <dusgud711@naver.com>
2021-12-16 16:38:25 +0900

    新しいファイルのjq.jsを作り、index.jsに書いたコードをjq.jsに移動しました！

---
+++ practice/07/try/src/js/jq.js
@@ -0,0 +1,47 @@
+(function() {
+    'use strict';
+
+    var form = $('#form');
+  
+    var formElements = {
+      name: form.find('input[name=name]'),
+      mail: form.find('input[name=mail]'),
+      gender: form.find('input:radio[name=gender]'),
+      age: form.find('select[name=age]'),
+      term: form.find('input:checkbox[name=term]')
+    };
+  
+    var errorText = $('#error');
+  
+    function isFormElementsFilled () {
+      if(formElements.name.val() == ''){
+        return false;
+      } else if (formElements.mail.val() == '') {
+        return false;
+      } else if (formElements.gender.is(':checked') == false) {
+        return false;
+      } else if (formElements.age.val()== '' ) { //.filter(':selected').text() == ''
+        return false;
+      } else if (formElements.term.is(':checked') == false) {
+        return false;
+      } else {
+        return true;
+      };
+    };
+  
+    function addActiveToError () {
+      errorText.addClass('is-active');
+    };
+  
+    form.on('submit', function (event) {
+      event.preventDefault();
+  
+      if(isFormElementsFilled()) {
+        form[0].submit();
+      } else {
+        addActiveToError();
+      }
+    });
+  })();
+
+// formElements.age.is(':selected')は選択されているかどうかではなくて、デフォルトで選択するoptionを指定しているだけ！
