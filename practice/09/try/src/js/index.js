2292c250d43cceecf6a0da47fdf0ccfa29e0e9ef
DESKTOP-N4LQJKL\dusgu <dusgud711@naver.com>
2022-01-06 18:31:20 +0900

    javascriptでpractice09 (ギャラリーの実装)を実装しました！

--- practice/09/try/src/js/index.js
+++ practice/09/try/src/js/index.js
@@ -1,1 +1,20 @@
+(function () {
+  'use strict';
+
+  var mainImg = document.getElementsByClassName('main-img')[0];
+  var thumbImgs = document.getElementsByClassName('thumb-item-img');
+
+  function removeAllActiveClass() {
+    Array.prototype.forEach.call(thumbImgs, function (el) {
+      el.classList.remove('is-active');
+    });
+  }
+
+  Array.prototype.forEach.call(thumbImgs, function (currentImage) {
+    currentImage.addEventListener('mouseenter', () => {
+      removeAllActiveClass();
+      currentImage.classList.add('is-active');
+      mainImg.setAttribute('src', currentImage.getAttribute('src').replace('_small', '_large'));
+    });
   });
+})();
