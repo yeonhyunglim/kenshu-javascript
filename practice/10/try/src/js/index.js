763dffc90413ef83a38f727dc6cf22d4f337d8e9
DESKTOP-N4LQJKL\dusgu <dusgud711@naver.com>
2022-01-20 18:41:31 +0900

    practice10（カルーセルの実装）を実装しました！

--- practice/10/try/src/js/index.js
+++ practice/10/try/src/js/index.js
@@ -1,48 +1,66 @@
 (function () {
   'use strict';
-  const imageList = document.getElementsByClassName('list')[0];
-  const imageItem = document.getElementsByClassName('item');
-  const prev = document.getElementsByClassName('prev')[0];
+  //elements
+  const imageList = document.querySelector('.list');
   const next = document.getElementsByClassName('next')[0];
+  const prev = document.getElementsByClassName('prev')[0];
+  let imageItems = document.querySelectorAll('.item');
 
-  let imageIndex = 0;
-  let position = 0;
-  const imageWidth = 400;
-  const imageLength = imageItem.length;
+  //values
+  let currentIndex = 1;
+  const itemWidth = imageItems[currentIndex].clientWidth;
+  let itemLength = imageItems.length;
 
-  function prevImage() {
-    if (imageIndex > 0) {
-      position += imageWidth;
-      imageList.style.marginLeft = `${position}px`;
-      imageList.classList.add('is-transition');
-      imageIndex = imageIndex - 1;
-    } else {
-      imageList.insertBefore(imageList.lastElementChild, imageList.firstElementChild);
+  function makeClone() {
+    const firstClone = imageItems[0].cloneNode(true); // deep clone 1st item after last item
+    const lastClone = imageItems[itemLength - 1].cloneNode(true); // deep clone last item before 1st item
+
+    firstClone.id = 'first-clone';
+    lastClone.id = 'last-clone';
+
+    imageList.append(firstClone);
+    imageList.prepend(lastClone);
+  }
+
+  function setFirstNode() {
+    imageList.style.marginLeft = `-${itemWidth}px`;
+  }
+
+  function moveToNextItem() {
+    if (currentIndex >= itemLength - 1) return;
+    currentIndex++;
+    imageList.style.marginLeft = `-${itemWidth * currentIndex}px`;
+    imageList.classList.add('is-transition');
+  }
+
+  function moveToPrevItem() {
+    if (currentIndex <= 0) return;
+    currentIndex--;
+    imageList.style.marginLeft = `-${itemWidth * currentIndex}px`;
+    imageList.classList.add('is-transition');
+  }
+
+  // transition end
+  imageList.addEventListener('transitionend', () => {
+    if (imageItems[currentIndex].id === 'first-clone') {
+      currentIndex = 1;
+      imageList.classList.remove('is-transition');
+      imageList.style.marginLeft = `-${currentIndex * itemWidth}px`;
     }
-  }
-  function nextImage() {
-    if (imageIndex < imageLength - 1) {
-      position -= imageWidth;
-      imageList.style.marginLeft = `${position}px`;
-      imageList.classList.add('is-transition');
-      imageIndex = imageIndex + 1;
-    } else {
-      imageList.appendChild(imageList.firstElementChild);
+    if (imageItems[currentIndex].id === 'last-clone') {
+      currentIndex = itemLength - 2;
+      imageList.classList.remove('is-transition');
+      imageList.style.marginLeft = `-${currentIndex * itemWidth}px`;
     }
-  }
-  function init() {
-    prev.addEventListener('click', prevImage);
-    next.addEventListener('click', nextImage);
-  }
-  init();
+  });
+
+  makeClone();
+
+  setFirstNode();
+
+  imageItems = document.querySelectorAll('.item');
+  itemLength = imageItems.length;
+
+  next.addEventListener('click', moveToNextItem);
+  prev.addEventListener('click', moveToPrevItem);
 })();
-
-// 1에서 3, 3에서 1 아직 구현 안함
-/*  function endTransition(prevOrNext) {
-    imageList.removeAttribute('style');
-    prevOrNext == 'prev' ? imageList.insertBefore(imageList.lastElementChild, imageList.firstElementChild) : imageList.appendChild(imageList.firstElementChild);
-  } */
-/*     imageList.ontransitioned = () => {
-      endTransition; */
-/*   function endTransition(prevOrNext) {
-    imageList.removeAttribute('style'); */
