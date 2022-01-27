0ab75c6b0f87143fd93667ec96b3b32266f7c50b
DESKTOP-N4LQJKL\dusgu <dusgud711@naver.com>
2022-01-27 11:48:02 +0900

    `class="prev"` をクリックしたら前の要素が、`class="next"` をクリックしたら次の要素を表示するコードを書きました。

--- practice/10/try/src/js/jq.js
+++ practice/10/try/src/js/jq.js
@@ -20,12 +20,45 @@ $(function () {
     imageList.css('margin-left', '-' + itemWidth + 'px');
   }
 
-  function moveToNextItem() {}
+  function moveToNextItem() {
+    if (currentIndex >= itemLength - 1) return;
+    currentIndex++;
+    imageList.addClass('is-transition');
+    imageList.css('margin-left', '-' + currentIndex * itemWidth + 'px');
+  }
 
-  function moveToPrevItem() {}
+  function moveToPrevItem() {
+    if (currentIndex <= 0) return;
+    currentIndex--;
+    imageList.addClass('is-transition');
+    imageList.css('margin-left', '-' + currentIndex * itemWidth + 'px');
+  }
 
-  function transitionEnd() {}
+  function transitionEnd() {
+    if (currentIndex == itemLength - 1) {
+      currentIndex = 1;
+      imageList.removeClass('is-transition');
+      imageList.css('margin-left', '-' + currentIndex * itemWidth + 'px');
+    }
+    if (currentIndex == 0) {
+      currentIndex = itemLength - 2;
+      imageList.removeClass('is-transition');
+      imageList.css('margin-left', '-' + currentIndex * itemWidth + 'px');
+    }
+  }
 
+  // Call functions
   makeClone();
   setFirstNode();
+
+  // Update Variables
+  imageItems = $('.item');
+  itemLength = imageItems.length;
+
+  // carousel event
+  next.on('click', moveToNextItem);
+  prev.on('click', moveToPrevItem);
+
+  // transition end event
+  imageList.on('transitionend', transitionEnd);
 });
